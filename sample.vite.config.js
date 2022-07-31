226b4757ceb0f7f2dc8a718506fd7d6c65473053
Ewan Breakey <ebreakey@gmail.com>
2022-08-01 04:02:04 +1000

    Setup command for building sample

---
+++ sample.vite.config.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react({ jsxRuntime: 'classic' })],
+  resolve: {
+    alias: {
+      '@stevent-team/react-party': resolve(__dirname, '/lib/main.js')
+    }
+  },
+})
