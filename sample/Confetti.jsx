d074f965370985d334b868813587661bafaf5969
Ewan Breakey <ebreakey@gmail.com>
2022-08-09 15:43:02 +1000

    Setup crescent shape and shape grid

---
+++ sample/Confetti.jsx
@@ -0,0 +1,89 @@
+import { useRef, useEffect } from 'react'
+import { useControls, folder, button } from 'leva'
+import {
+  useConfetti,
+  DEFAULT_SHAPE_FUNCTIONS,
+  loadImage,
+  drawImageParticle,
+} from '@stevent-team/react-party'
+
+// Sample SVG image
+const SAMPLE_IMAGE = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 36 36'%3E%3Cpath fill='%23DD2E44' d='M35.885 11.833c0-5.45-4.418-9.868-9.867-9.868-3.308 0-6.227 1.633-8.018 4.129-1.791-2.496-4.71-4.129-8.017-4.129-5.45 0-9.868 4.417-9.868 9.868 0 .772.098 1.52.266 2.241C1.751 22.587 11.216 31.568 18 34.034c6.783-2.466 16.249-11.447 17.617-19.959.17-.721.268-1.469.268-2.242z'/%3E%3Cpath fill='%23FDCB58' d='M34.347 23.894l-3.824-1.416-1.416-3.824c-.145-.394-.52-.654-.938-.654-.418 0-.793.26-.938.653l-1.416 3.824-3.824 1.416c-.393.144-.653.519-.653.938 0 .418.261.793.653.938l3.824 1.416 1.416 3.824c.145.393.52.653.938.653.418 0 .793-.261.938-.653l1.416-3.824 3.824-1.416c.392-.145.653-.52.653-.938 0-.418-.261-.793-.653-.937zm-23-16.001l-2.365-.875-.875-2.365C7.961 4.26 7.587 4 7.169 4c-.419 0-.793.26-.938.653l-.876 2.365-2.364.875c-.393.146-.653.52-.653.938 0 .418.26.792.653.938l2.365.875.875 2.365c.146.393.52.653.938.653.418 0 .792-.26.938-.653l.875-2.365 2.365-.875c.393-.146.653-.519.653-.938 0-.418-.26-.792-.653-.938z'/%3E%3C/svg%3E"
+
+const Confetti = () => {
+  // Setup controls
+  const launch = useControls('Creation options', {
+    duration: { value: 0, min: 0, max: 5000 },
+    count: { value: 75, min: 1, max: 2000, step: 1 },
+  })
+
+  const global = useControls('Environment options', {
+    gravity: { value: 9.8, min: -50, max: 50, step: .1 },
+    wind: { value: 0, min: -1, max: 1 },
+    speed: { value: 1, min: 0, max: 10 },
+    killDistance: { value: 100, min: 0, max: 3000, step: 10 },
+  }, { collapsed: true })
+
+  const confetti = useControls('Movement options', {
+    initialVelocity: [0, -3],
+    initialVelocitySpread: [5, 7],
+    diameter: { value: [10, 30], min: 0, max: 100 },
+    twirl: { value: [0, .2], min: 0, max: 1 },
+    angle: folder({
+      initialAngle: { value: [0, 360], min: 0, max: 360 },
+      angleIncrement: { value: [-10, 10], min: -50, max: 50 },
+    }, { collapsed: true }),
+    flip: folder({
+      initialFlip: { value: [0, 360], min: 0, max: 360 },
+      flipIncrement: { value: [-10, 10], min: -50, max: 50 },
+    }, { collapsed: true }),
+    rotationVelocityCoefficient: { value: .8, min: 0, max: 5 },
+  }, { collapsed: true })
+
+  const { 'Explore shape options': _btn, ...shapeWeights }= useControls('Shape weights', {
+    'Explore shape options': button(() => window.open('/shapes')),
+    'moon': { value: 0, min: 0, max: 5, step: 1 },
+    'line': { value: 0, min: 0, max: 5, step: 1 },
+    'circle': { value: 1, min: 0, max: 5, step: 1 },
+    'triangle': { value: 1, min: 0, max: 5, step: 1 },
+    'square': { value: 1, min: 0, max: 5, step: 1 },
+    'pentagon': { value: 0, min: 0, max: 5, step: 1 },
+    'hexagon': { value: 0, min: 0, max: 5, step: 1 },
+    'heptagon': { value: 0, min: 0, max: 5, step: 1 },
+    'octagon': { value: 0, min: 0, max: 5, step: 1 },
+    'diamond': { value: 0, min: 0, max: 5, step: 1 },
+    'star': { value: 1, min: 0, max: 5, step: 1 },
+    'hexagram': { value: 0, min: 0, max: 5, step: 1 },
+    'image': { value: 0, min: 0, max: 5, step: 1 },
+  }, { collapsed: true })
+
+  useControls({
+    'Visit the GitHub': button(() => window.open('https://github.com/stevent-team/react-party')),
+  })
+
+  // Load custom image
+  const imageRef = useRef()
+  useEffect(() => {
+    loadImage(SAMPLE_IMAGE)
+      .then(img => imageRef.current = img)
+  }, [])
+
+  // Call hook
+  const { createConfetti, canvasProps } = useConfetti({
+    ...global,
+    ...confetti,
+    shapeWeights,
+
+    shapeFunctions: {
+      ...DEFAULT_SHAPE_FUNCTIONS,
+      image: drawImageParticle(imageRef.current),
+    }
+  })
+
+  return <>
+    <canvas {...canvasProps} />
+    <button onClick={() => createConfetti(launch)}>Party!</button>
+  </>
+}
+
+export default Confetti
---
+++ sample/ShapeGrid.jsx
@@ -0,0 +1,147 @@
+import { useControls } from 'leva'
+import { useEffect, useRef, useCallback } from 'react'
+import { DEFAULT_SHAPE_FUNCTIONS } from '@stevent-team/react-party'
+import { useAnimationFrame } from '/lib/util'
+import { drawCrescentParticle, drawRegularPolygonParticle } from '/lib'
+
+const gridStyle = {
+  width: '100%',
+  height: '100%',
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr 1fr 1fr',
+  margin: 0,
+  padding: '3em',
+  paddingRight: '20em',
+  boxSizing: 'border-box',
+}
+
+const cellStyle = {
+  position: 'relative',
+  width: '100%',
+  height: '100%',
+  aspectRatio: '1',
+  outline: '1px solid grey',
+}
+
+const canvasStyle = {
+  position: 'absolute',
+  inset: 0,
+  width: '100%',
+  height: '100%',
+}
+
+const ShapeGrid = () => {
+  const genericOptions = useControls('Generic options', {
+    diameterPortion: { value: 0.9, min: 0, max: 2 },
+    flip: { value: 0, min: 0, max: 360 },
+    angle: { value: 0, min: 0, max: 360 },
+    twirl: { value: 0, min: 0, max: 1 },
+    color: 'white',
+  })
+
+  const { color: polygonColor, ...polygonOptions }= useControls('Dynamic Polygon Options', {
+    numPoints: { value: 3, min: 1, step: 1, max: 20 },
+    startAngle: { value: 0, min: 0, max: 6.28 },
+    spiked: false,
+    spikedPortion: { value: .5, min: 0, max: 1 },
+    color: '#64c0de'
+  })
+
+  const { color: crescentColor, ...crescentOptions }= useControls('Dynamic Crescent Options', {
+    forwardPortion: { value: .5, min: 0, max: 2 },
+    backwardPortion: { value: 1, min: 0, max: 2 },
+    horizontalShiftPortion: { value: 0.4, min: 0, max: 1 },
+    verticalShiftPortion: { value: 0.25, min: 0, max: 1 },
+    innerControlPortion: { value: 0.6, min: 0, max: 2 },
+    outerControlPortion: { value: 1, min: 0, max: 2 },
+    // verticalPullInAmount: { value: .4, min: 0, max: 1 },
+    // verticalSquishAmount: { value: 2, min: 0, max: 1 },
+    color: '#64c0de'
+  })
+
+  return <div style={gridStyle}>
+    {/* Dynamic polygon */}
+    <ShapeCell shapeFn={drawRegularPolygonParticle(polygonOptions)} {...genericOptions} color={polygonColor} />
+
+    {/* Dynamic crescent */}
+    <ShapeCell shapeFn={drawCrescentParticle(crescentOptions)} {...genericOptions} color={crescentColor} />
+
+    {/* Built-in shapes */}
+    {Object.keys(DEFAULT_SHAPE_FUNCTIONS).map(shape =>
+      <ShapeCell shapeFn={DEFAULT_SHAPE_FUNCTIONS[shape]} key={shape} {...genericOptions} />)}
+  </div>
+}
+
+const ShapeCell = ({
+  shapeFn=DEFAULT_SHAPE_FUNCTIONS.triangle,
+  color='white',
+  diameterPortion=.9,
+  flip=0,
+  angle=0,
+  twirl=0,
+}) => {
+  const canvasRef = useRef()
+  const canvasBBoxRef = useRef()
+  const ctxRef = useRef()
+
+  const onCanvasResize = useCallback(() => {
+    if (canvasRef.current) {
+      // Get canvas bounding box
+      const canvasBBox = canvasRef.current.getBoundingClientRect()
+      const { width, height } = canvasBBox
+      canvasBBoxRef.current = canvasBBox
+
+      // Set canvas size
+      const scale = window.devicePixelRatio
+      canvasRef.current.width = Math.floor(width * scale)
+      canvasRef.current.height = Math.floor(height * scale)
+    }
+  }, [])
+
+  // Observe for canvas size changes
+  useEffect(() => {
+    if (canvasRef.current) {
+      // Get canvas context
+      ctxRef.current = canvasRef.current.getContext('2d')
+
+      // Observe for canvas size changes
+      const resizeObserver = new ResizeObserver(onCanvasResize)
+      resizeObserver.observe(canvasRef.current)
+
+      return () => resizeObserver.disconnect()
+    }
+  }, [])
+
+  useAnimationFrame(() => {
+    if (ctxRef.current && canvasBBoxRef.current) {
+      const { width, height } = canvasRef.current
+      const bb = canvasBBoxRef.current
+
+      // Clear canvas
+      ctxRef.current.clearRect(0, 0, width, height)
+
+      // Render particle
+      ctxRef.current.setTransform(
+        new DOMMatrixReadOnly()
+          .translate(width/2, height/2)
+          .scale(window.devicePixelRatio)
+          .rotate(angle)
+          .scale(1, Math.sin(((flip+90)*(Math.PI/180))))
+      )
+      shapeFn({ ctx: ctxRef.current, p: {
+        flip,
+        angle,
+        twirl,
+        diameter: Math.max(bb.width, bb.height) * diameterPortion,
+        color,
+      }})
+      ctxRef.current.setTransform(1,0,0,1,0,0)
+    }
+  })
+
+  return <div style={cellStyle}>
+    <canvas ref={canvasRef} style={canvasStyle} />
+  </div>
+}
+
+export default ShapeGrid
