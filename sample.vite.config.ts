a0e729b2098a2ae913c64b7db7084dd5167c82e9
Ewan Breakey <ebreakey@gmail.com>
2023-10-15 16:20:34 +1100

    Change to automatic jsx runtime

--- sample.vite.config.ts
+++ sample.vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react({ jsxRuntime: 'classic' })],
+  plugins: [react()],
   resolve: {
     alias: {
       '@stevent-team/react-party': resolve(__dirname, 'lib/index.ts')
